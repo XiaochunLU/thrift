e1030e8fc1bab04f9461f2f4a6b626ec26a36966
XiaochunLU <robert1111@gmail.com>
2014-06-11 14:21:17 +0800

    Initial commit of thrift for Vert.x java library
    This is the first implementation of thrift for Vert.x java library. It
    contains only one server: TEventBusServer, which transports messages
    over Vert.x Event Bus.

---
+++ lib/vertx-java/src/main/java/org/apache/thrift/async/TAsyncMethodCall.java
@@ -0,0 +1,198 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.thrift.async;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.thrift.TApplicationException;
+import org.apache.thrift.TBase;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TMessage;
+import org.apache.thrift.protocol.TMessageType;
+import org.apache.thrift.protocol.TProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.vertx.java.core.AsyncResult;
+import org.vertx.java.core.AsyncResultHandler;
+
+/**
+ * Encapsulates an async method call
+ * Need to generate:
+ *   - protected void write_args(TProtocol out) throws TException
+ *   - protected T getResult() throws TException
+ * @param <T>
+ */
+public abstract class TAsyncMethodCall<T> {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TAsyncMethodCall.class.getName());
+
+  // We have to ensure that different method calls have different seqid's
+  private static final AtomicInteger seqidGenerator = new AtomicInteger(0);
+  
+  private final TAsyncClientManager clientManager;
+  private final AsyncResultHandler<T> handler;
+  private final boolean isOneway;
+
+  private final int seqid;
+
+  protected TAsyncMethodCall(TAsyncClientManager clientManager, AsyncResultHandler<T> handler, boolean isOneway) {
+    this.clientManager = clientManager;
+    this.handler = handler;
+    this.isOneway = isOneway;
+    
+    seqid = seqidGenerator.incrementAndGet();
+  }
+
+  public void start(TProtocol oprot) {
+    Exception cause = null;
+    try {
+      write_args(oprot);
+      oprot.getTransport().flush();
+    } catch (TException te) {
+      LOGGER.error("Thrift error occurred during sending the message", te);
+      cause = te;
+    } catch (Exception e) {
+      LOGGER.error("Error occurred during sending the message.", e);
+      cause = e;
+    }
+    if (cause != null) {
+      onError(cause);
+      return;
+    }
+    if (isOneway) {
+      handler.handle(new SucceededResult<T>());
+      onComplete();
+    }
+  }
+
+  protected abstract void write_args(TProtocol oprot) throws TException;
+
+  public void responseReady(TProtocol iprot) {
+    T result = null;
+    Exception cause = null;
+    try {
+      result = getResult(iprot);
+    } catch (TException te) {
+      LOGGER.error("Thrift error occurred during recving response", te);
+      cause = te;
+    } catch (Exception e) {
+      LOGGER.error("Error occurred during recving response", e);
+      onError(e);
+      return;
+    }
+    if (cause != null) {
+      handler.handle(new FailedResult<T>(cause));
+    } else {
+      handler.handle(new SucceededResult<T>(result));
+    }
+    onComplete();
+  }
+
+  protected abstract T getResult(TProtocol iprot) throws TException;
+
+  @SuppressWarnings("rawtypes")
+  protected void sendBase(TProtocol oprot, String methodName, TBase args) throws TException {
+    oprot.writeMessageBegin(new TMessage(methodName, TMessageType.CALL, seqid));
+    args.write(oprot);
+    oprot.writeMessageEnd();
+    oprot.getTransport().flush();
+  }
+
+  @SuppressWarnings("rawtypes")
+  protected void receiveBase(TProtocol iprot, TBase result, String methodName) throws TException {
+    TMessage msg = iprot.readMessageBegin();
+    if (msg.type == TMessageType.EXCEPTION) {
+      TApplicationException x = TApplicationException.read(iprot);
+      iprot.readMessageEnd();
+      throw x;
+    }
+    result.read(iprot);
+    iprot.readMessageEnd();
+  }
+
+  private void onComplete() {
+    clientManager.unregisterMethodCall(seqid, this);
+  }
+
+  private void onError(Exception e) {
+    handler.handle(new FailedResult<T>(e));
+    clientManager.unregisterMethodCall(seqid, this);
+  }
+
+  private static final class SucceededResult<U> implements AsyncResult<U> {
+    private final U result;
+
+    SucceededResult() {
+      result = null;
+    }
+
+    SucceededResult(U result) {
+      this.result = result;
+    }
+
+    @Override
+    public U result() {
+      return result;
+    }
+
+    @Override
+    public Throwable cause() {
+      return null;
+    }
+
+    @Override
+    public boolean succeeded() {
+      return true;
+    }
+
+    @Override
+    public boolean failed() {
+      return false;
+    }
+  }
+  
+  private static final class FailedResult<U> implements AsyncResult<U> {
+    final Throwable cause;
+
+    FailedResult(Throwable cause) {
+      this.cause = cause;
+    }
+
+    @Override
+    public U result() {
+      return null;
+    }
+
+    @Override
+    public Throwable cause() {
+      return cause;
+    }
+
+    @Override
+    public boolean succeeded() {
+      return false;
+    }
+
+    @Override
+    public boolean failed() {
+      return true;
+    }
+  }
+  
+}
